d1c353ec92301e10cb1a00ad327110e03a27e27f
iammana2004 <azarnooshmana1383@gmail.com>
2025-04-25 03:05:37 +0330

    simple TCP chat

---
+++ client.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+user.connect(('127.0.0.1', 15000))
+
+name = input("Enter your nickname: ")
+
+def receive():
+    while True:
+        try:
+            message = user.recv(1024).decode('ascii')
+            if message == 'Name':
+                user.send(name.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            user.close()
+            break
+
+def write():
+    global name
+    while True:
+        message = input("")
+        if message.lower() == 'change':
+            user.send('change'.encode('ascii'))
+            print(user.recv(1024).decode('ascii'))
+            name = input("Enter your new username: ")
+            user.send(name.encode('ascii'))
+        elif message.lower() == 'list':
+            user.send('list'.encode('ascii'))
+            print(user.recv(1024).decode('ascii'))
+        elif message.lower() == 'exit':
+            print("Exiting the chat...")
+            user.send('exit'.encode('ascii'))
+            user.close()
+            break
+        else:
+            user.send(f'{name}: {message}'.encode('ascii'))
+
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
---
+++ server.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+
+host = '127.0.0.1' #192.168.87.138
+port = 15000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+users = []
+names = []
+
+def show(message):
+    for user in users:
+        try:
+            user.send(message)
+        except:
+            users.remove(user)
+
+def manage(user):
+    while True:
+        try:
+            message = user.recv(1024).decode('ascii')
+            if message.lower() == 'change':
+                user.send('Enter your new username:'.encode('ascii'))
+                new_name = user.recv(1024).decode('ascii')
+                index = users.index(user)
+                old_name = names[index]
+                names[index] = new_name
+                show(f'{old_name} changed their username to {new_name}'.encode('ascii'))
+            elif message.lower() == 'list':
+                user_list = ', '.join(names)
+                user.send(f'Users in the chatroom: {user_list}'.encode('ascii'))
+            else:
+                show(message.encode('ascii'))
+        except:
+            index = users.index(user)
+            users.remove(user)
+            user.close()
+            name = names[index]
+            names.remove(name)
+            show(f'{name} left the chat.'.encode('ascii'))
+            break
+        
+def receive():
+    while True:
+        user, address = server.accept()
+        print(f"connected with {address}")
+
+        user.send('Name'.encode('ascii'))
+        name = user.recv(1024).decode('ascii')
+        names.append(name)
+        users.append(user)
+
+        print(f'name of the user is {name}')
+        show(f'{name} joined the chat'. encode('ascii'))
+        user.send('connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target=manage, args=(user,))
+        thread.start()
+
+print("server listening...")
+receive()
+
